2dd889155b579fc387158d9fd57986e33b793141
Vladimir Gusev <bo-ba@mail.ru>
2014-02-09 21:59:34 -0500

    normalized fft to u char

--- circuitShader/circuitShader.cpp
+++ circuitShader/circuitShader.cpp
@@ -72,19 +72,33 @@ void circuitShaderApp::update()
 		return;
 	}
     
+    /*
+     The FFT signal, which is 512 pixels/frequencies long, gets normalized to 0..1 and mapped to 0..255. The wave form, which is also 512 pixels/sampled long, gets renormalized too from -16387..16384 to 0..1. FFT goes in the first row, waveform in the second row. So this is a 512x2 gray scale 8 bit texture.
+     */
+    
 	uint16_t bandCount = 512;
 	mFftDataRef = audio::calculateFft( mPcmBuffer->getChannelData( audio::CHANNEL_FRONT_LEFT ), bandCount );
     
     float * fftBuffer = mFftDataRef.get();
     if (!fftBuffer) return;
-    float ht = 1.0f;
+
 	
     //create a sound texture as 512x2
 	unsigned char signal[1024];
     
     //the first row is the spectrum (shadertoy)
-	for(int i=0;i<512;++i)
-		signal[i] = (unsigned char) (fftBuffer[i] / bandCount * ht);
+    float max = 0.;
+    for(int i=0;i<512;++i){
+		if (fftBuffer[i] > max) max = fftBuffer[i];
+    }
+
+    float ht = 255.0/max;
+    unsigned char ax = 0;
+	for(int i=0;i<512;++i){
+		signal[i] = (unsigned char) (fftBuffer[i] * ht);
+        if (signal[i] > ax) ax = signal[i];
+    }
+    std::cout<<(int)ax<<std::endl;
 	
 	// the second is waveform (shadertoy)
 	for(int i=0;i<512;++i)
